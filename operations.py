30a799dae9abe0bc36aca29c30b070daed852ce2
Naveenbandari26 <bandarin29@gmail.com>
2025-04-16 22:08:58 +0530

    Create operations.py

---
+++ operations.py
@@ -0,0 +1,32 @@
+#create a program that have five functions of maths 
+# 1. add
+# 2. subtract
+# 3. multiply
+# 4. divide
+# 5. modulus
+
+#give the code
+
+def add(a, b):
+    
+
+def subtract(a, b):
+    
+
+def multiply(a, b):
+   
+   
+def divide(a, b):
+    
+
+def modulus(a, b):
+    
+
+# Test the functions
+a,b=map(int,input("Enter two numbers: ").split())
+print("Addition:", add(a, b))
+print("Subtraction:", subtract(a, b))
+print("Multiplication:", multiply(a, b))
+print("Division:", divide(a, b))
+print("Modulus:", modulus(a, b))
+
